741dca1b7a726f0f19f5d67d1974049b02a6bcb5
Marcos Silva (PSV) <marcos.silva@psvsistemas.com.br>
2022-05-06 15:39:08 -0300

    feat: final project

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer'
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "9dc887e083b034",
+    pass: "8652a0c27740f5"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Marcos Vinícius <mviniciusess@gmail.com>',
+      subject,
+      html: body
+    })
+  }
+}
